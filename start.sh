894c961adae93ed4dd046276033953335c2fa943
Ankush Yadav <138442615+Mswpresents@users.noreply.github.com>
2024-04-23 12:29:38 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,12 +5,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/VJBots/VJ-FILTER-BOT.git /VJ-FILTER-BOT 
+  git clone https://github.com/Mswpresents/Advance-Filter-Bot.git /Advance-Filter-Bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /VJ-FILTER-BOT 
+  git clone $UPSTREAM_REPO /Advance-Filter-Bot 
 fi
-cd /VJ-FILTER-BOT 
+cd /Advance-Filter-Bot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
